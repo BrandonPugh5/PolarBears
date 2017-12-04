19226e46133025c4d6d012e04bf899315eaa8342
Brandon Pugh <pughb5@students.rowan.edu>
2017-12-03 23:21:34 -0500

    'initial commit'

---
+++ PlayView/Motor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarBearsProgram
+{
+    public class Motor
+    {
+        string ip;
+        int rotation_limit;
+        int accel_limit;
+        int velocity_limit;
+        int decel_limit;
+        public String serial;
+
+        public Motor(String IP, String serial, int Acceleration,int Deceleration, int Velocity)
+        {
+            this.ip = IP;
+            //this.rotation_limit = Rotation;
+            this.serial = serial;
+            this.accel_limit = Acceleration;
+            this.decel_limit = Deceleration;
+            this.velocity_limit = Velocity;
+        
+        }
+
+        public string IP()
+        {
+            return ip;
+        }
+
+        public int Rotation_Limit()
+        {
+            return rotation_limit;
+        }
+        public int Acceleration_Limit()
+        {
+            return accel_limit;
+        }
+        public int Deceleration_Limit()
+        {
+            return decel_limit;
+        }
+
+        public int Velocity_Limit()
+        {
+            return velocity_limit;
+        }
+
+        public override string ToString()
+        {
+            return serial;
+        }
+    }
+}
+

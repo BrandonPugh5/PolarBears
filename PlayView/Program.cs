f13d0b133c52f912038d28371de31e30c4b654bc
Brandon Pugh <pughb5@students.rowan.edu>
2017-12-07 12:19:36 -0500

    Edited Spacing

---
+++ PlayView/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolarBearsProgram
+{
+    static class Program
+    {
+        public static PlayView playView;
+        public static Cue_Editor cueEdit;
+        public static MotorConfiguration motorConf;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            playView = new PlayView();
+            cueEdit = new Cue_Editor();
+            cueEdit.Hide();
+            motorConf = new MotorConfiguration();
+            motorConf.Hide();
+            Application.Run(playView);
+        }
+    }
+}

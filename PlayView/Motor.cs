12faf697bec6f205548dfb34de321dd2787fd8ac
Brandon Pugh <pughb5@students.rowan.edu>
2017-12-14 00:57:49 -0500

    Final Project Checkout

---
+++ PlayView/Motor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarBearsProgram
+{
+    public class Motor
+    {
+       
+        int rotation_limit;
+        int accel_limit;
+        int velocity_limit;
+        int decel_limit;
+        public string ip { get; set; }
+        public int AccelLimit { get; set; }
+        public int VelocityLimit { get; set; }
+        public int DecelLimit { get; set; }
+        public int RotationSoftLimit { get; set; }
+        public int AccelSoftLimit { get; set; }
+        public int DecelSoftLimit { get; set; }
+        public int VelocitySoftLimit { get; set; }
+        public String serial;
+
+        public Motor(String ip, String Serial, int AccelLimit, int DecelLimit, int VelocityLimit,/* int RotationSoftLimit*/ int AccelSoftLimit, int DecelSoftLimit, int VelocitySoftLimit)
+        {
+            this.ip = ip;
+            this.serial = Serial;
+            this.AccelLimit = AccelLimit;
+            this.DecelLimit = DecelLimit;
+            this.VelocityLimit = VelocityLimit;
+            this.RotationSoftLimit = RotationSoftLimit;
+            this.AccelSoftLimit = AccelSoftLimit;
+            this.DecelSoftLimit = DecelSoftLimit;
+            this.VelocitySoftLimit = VelocitySoftLimit;
+        }
+
+        public string IP()
+        {
+            return ip;
+        }
+
+        public int Rotation_Limit()
+        {
+            return rotation_limit;
+        }
+        public int Acceleration_Limit()
+        {
+            return accel_limit;
+        }
+        public int Deceleration_Limit()
+        {
+            return decel_limit;
+        }
+
+        public int Velocity_Limit()
+        {
+            return velocity_limit;
+        }
+
+        public override string ToString()
+        {
+            return serial;
+        }
+    }
+}
+
---
+++ PlayView/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolarBearsProgram
+{
+    static class Program
+    {
+        public static PlayView playView;
+        public static Cue_Editor cueEdit;
+        public static MotorConfiguration motorConf;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Application.Run(p);
+            playView = new PlayView();
+            cueEdit = new Cue_Editor();
+            cueEdit.Hide();
+            motorConf = new MotorConfiguration();
+            motorConf.Hide();
+            Application.Run(playView);
+        }
+    }
+}
